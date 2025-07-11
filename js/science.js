ae7f0e1457bdbccf3085ab185162cd116adf4eae
baba01254444 <160570191+baba01254444@users.noreply.github.com>
2025-07-11 07:25:50 +0530

    Create science.js

---
+++ js/science.js
@@ -0,0 +1,34 @@
+const scienceQuestions = [
+  {
+    q: "Q1. इस चित्र को देखकर उत्तर दें:<br><img src='images/science_img01.jpg' width='300'>",
+    o: ["विकल्प 1 (Option 1)", "विकल्प 2 (Option 2)", "विकल्प 3 (Option 3)", "विकल्प 4 (Option 4)"],
+    a: 0
+  },
+  {
+    q: "Q2. इस चित्र को देखकर उत्तर दें:<br><img src='images/science_img02.jpg' width='300'>",
+    o: ["विकल्प 1", "विकल्प 2", "विकल्प 3", "विकल्प 4"],
+    a: 0
+  },
+  {
+    q: "Q3. इस चित्र को देखकर उत्तर दें:<br><img src='images/science_img03.jpg' width='300'>",
+    o: ["Option 1", "Option 2", "Option 3", "Option 4"],
+    a: 0
+  },
+  // ... Q4 to Q15 similarly with images
+  {
+    q: "Q16. वोल्टेज का SI unit क्या है? / What is the SI unit of voltage?",
+    o: ["वोल्ट (Volt)", "ओम (Ohm)", "एम्पीयर (Ampere)", "वॉट (Watt)"],
+    a: 0
+  },
+  {
+    q: "Q17. न्यूटन का SI unit किसके लिए है? / Newton is the SI unit of?",
+    o: ["बल (Force)", "ऊर्जा (Energy)", "कार्य (Work)", "दबाव (Pressure)"],
+    a: 0
+  },
+  {
+    q: "Q18. Ohm's law किससे संबंधित है? / Ohm's law relates to?",
+    o: ["वोल्टेज और करंट (Voltage & Current)", "ऊर्जा और समय", "गति और दूरी", "दाब और तापमान"],
+    a: 0
+  }
+  // ... Add up to Q150 like this
+];
